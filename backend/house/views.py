4c51c90d04e26d6e0febfeb2dc83d03e7f64e14b
yuhsuanyang <sb0953330882@gmail.com>
2022-10-22 11:41:27 +0800

    feat(backend): api for sections

--- backend/house/views.py
+++ backend/house/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .serializers import ClusterSerializer
 from .models import House
+from .config import section
 
 # Create your views here.
 
@@ -14,6 +15,16 @@ def get_cluster(request, cluster_id):
     serializer = ClusterSerializer(data, many=True)
     return JsonResponse(serializer.data,
                         safe=False,
+                        json_dumps_params={
+                            'ensure_ascii': False,
+                            'indent': 2
+                        })
+
+
+@csrf_exempt
+def get_section(requsest):
+    city_sections = {city: list(section[city].keys()) for city in section}
+    return JsonResponse(city_sections,
                         json_dumps_params={
                             'ensure_ascii': False,
                             'indent': 2
