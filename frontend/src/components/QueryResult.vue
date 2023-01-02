a97b650c34210a8c870fa5e874c8cd16ff01e308
yuhsuanyang <sb0953330882@gmail.com>
2023-01-02 13:29:37 +0800

    feat(fronend): thumbnail with price label

--- frontend/src/components/QueryResult.vue
+++ frontend/src/components/QueryResult.vue
@@ -1,22 +1,33 @@
 <template>
-  <div class="content">
+  <div>
     <div class="nav">
       <div>
         <router-link to="/"
           ><img src="../assets/logo_bird.png" class="logo"
         /></router-link>
       </div>
-      <div>
-        <p>查看喜歡</p>
-      </div>
-      <div>
-        <p>排序</p>
-      </div>
+      <div><p>查看喜歡</p></div>
+      <div><p>排序</p></div>
     </div>
-    <div class="container">
-        <div v-for="i in clusters" :key="i.id">
-          <Thumbnail :img_src="i['img_url'][0]"/>
+    <div class="content">
+      <div class="description">
+        <p>台北市, 文山區, 屋齡0~20年, 權狀0~40坪, 不限格局, 共7個物件</p>
+      </div>
+      <div style="display: flex">
+        <div style="width: 200px; height: 80vh">
+          <div class="top">
+            <img src="../assets/top.png" style="width: 30%" />
+          </div>
         </div>
+        <div class="container">
+          <div v-for="item in clusters" :key="item.id">
+            <Thumbnail
+              :img_src="item['img_url'][sample(item['img_url'])]"
+              :price="item['price']"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,28 +44,37 @@ export default {
   },
   data() {
     return {
-        img_url: "",
-        clusterId: [],
-        clusters: {},
+      img_url: "",
+      clusterId: [],
+      clusters: {},
     };
   },
+  methods: {
+    sample(arr) {
+      var x = Math.floor(Math.random() * arr.length);
+      return x;
+    },
+  },
   created() {
-    this.img_url=SampleData["1343"][0]["img_url"][0];
-    for (var key in SampleData){
-//        this.clusterId.push(key);
-        var data = SampleData[key]
-        this.clusters[key] = {
-            "address": data[0]["region_name"].concat(data[0]["section_name"], data[0]["address"]),
-            "price": data[0]["price"],
-            "room": data[0]["room"],
-            "floor": data[0]["floor"],
-            "houseage": data[0]["houseage"],
-            "img_url": data[0]["img_url"]
-        }
-        for (var i in data){
-            var item = data[i]
-            this.clusters[key]["img_url"].push(...item["img_url"])
-        }
+    this.img_url = SampleData["1343"][0]["img_url"][0];
+    for (var key in SampleData) {
+      //        this.clusterId.push(key);
+      var data = SampleData[key];
+      this.clusters[key] = {
+        address: data[0]["region_name"].concat(
+          data[0]["section_name"],
+          data[0]["address"]
+        ),
+        price: data[0]["price"],
+        room: data[0]["room"],
+        floor: data[0]["floor"],
+        houseage: data[0]["houseage"],
+        img_url: data[0]["img_url"],
+      };
+      for (var i in data) {
+        var item = data[i];
+        this.clusters[key]["img_url"].push(...item["img_url"]);
+      }
     }
     console.log(this.clusters);
   },
@@ -62,7 +82,7 @@ export default {
 </script>
 <style scoped>
 .content {
-  background-color: aliceblue;
+  background-color: #eff4fd;
   /*  display: flex;*/
 }
 .nav {
@@ -72,10 +92,10 @@ export default {
   border-bottom: 5px solid #faf8f8;
 }
 .logo {
-  height: 60px;
+  height: 10vh;
 }
 .nav > div {
-  height: 60px;
+  height: 10vh;
   display: flex;
   font-weight: bold;
 }
@@ -85,7 +105,20 @@ p {
 .container {
   display: flex;
   flex-wrap: wrap;
-  gap: 2%;
-  padding: 2% 5%;
+  align-content: flex-start;
+  padding-left: 10%;
+}
+.container > div {
+  margin: 3px;
+}
+.description {
+  font-size: 8px;
+  text-align: left;
+  padding: 2%;
+}
+.top {
+  position: fixed;
+  bottom: 5%;
+  left: 0;
 }
 </style>
--- frontend/src/components/Thumbnail.vue
+++ frontend/src/components/Thumbnail.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <img v-bind:src="img_src" height="230" width="350" />
+  <div class="boundary">
+    <div class="price_label">
+      <p>{{ price }}萬</p>
+    </div>
+    <img v-bind:src="img_src" height="100%" width="100%" />
   </div>
 </template>
 <script>
@@ -8,7 +11,28 @@ export default {
   name: "Thumbnail",
   props: {
     img_src: String,
+    price: Number,
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.boundary {
+  position: relative;
+  display: flex;
+  height: 230px;
+  width: 300px;
+}
+.price_label {
+  position: absolute;
+  background-color: #fa9b0e;
+  opacity: 0.75;
+  top: 5%;
+  font-size: 10px;
+  font-weight: 900;
+  padding: 0 3%;
+}
+.price_label > p {
+  margin: 1px 2px;
+  color: white;
+}
+</style>
